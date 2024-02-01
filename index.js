bfade39aeff761c977acb8292c1be5f6fdaffe17
LawrenceN3lson <hsc1325@outlook.com>
2024-02-01 18:01:48 +0800

    update index html and js

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ async function connect() {
     console.log("MetaMask is not installed!");
   }
 }
+
+async function fund() {
+  if (typeof window.ethereum !== undefined) {
+    // 想要用钱包发送一个交易，需要的内容
+    // provider RPC节点服务提供者
+    // signer / wallet 发送交易的签名人或钱包
+    // contract 可以交互的合约(需要知道ABI和合约地址)
+  } else {
+    console.log("MetaMask is not installed!");
+  }
+}
